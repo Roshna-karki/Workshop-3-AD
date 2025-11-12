8b4d20d96372da38a132040efaa692665418f7fc
Roshna-karki <xettrirsohna12@gmail.com>
2025-11-12 11:20:57 +0545

    Task 2

---
+++ Task 2/NullOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class NullOperations
+{
+    public void PerformNullChecks()
+    {
+        string username = null;
+
+        // Using Ternary Operator
+        Console.WriteLine(username == null ? "Username is not available" : username);
+
+        // Using Null-Coalescing Operator
+        Console.WriteLine(username ?? "Username is not available");
+
+        // Using Null-Coalescing Assignment Operator
+        username ??= "DefaultUser";
+        Console.WriteLine("Updated Username: " + username);
+    }
+}
