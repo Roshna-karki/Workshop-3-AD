d2ed3e0d8a77883a26665a51082455f7c37d071e
Roshna-karki <xettrirsohna12@gmail.com>
2025-11-12 11:11:19 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Operators op = new Operators();
+
+        op.Add(10, 5);
+        op.Subtract(10, 5);
+        op.Multiply(10, 5);
+        op.Divide(10, 5);
+        op.OddEvenFinder(7);
+    }
+}
