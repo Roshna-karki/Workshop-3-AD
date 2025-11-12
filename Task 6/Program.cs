57dc995c2ee83e0bafb8f967f60d1110bb053329
Roshna-karki <xettrirsohna12@gmail.com>
2025-11-12 12:04:27 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,42 @@
+﻿ class Program
+    {
+        public static void Main() 
+        {
+            Program program = new Program(); 
+            program.ConvertInput();
+
+            program.CheckPassword();
+        }
+        public void ConvertInput()
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You entered: " + number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format");
+            }
+            finally
+            {
+                Console.WriteLine("Program Executed");
+            }
+        }
+
+        public void CheckPassword()
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+            else
+            {
+                Console.WriteLine("Password satisfied the requirement");
+            }
+        }
+    }
